9283b3c0ead0b8ef2f424a04466f6c4b05920c16
Tomahawk2191 <polite.i@northeastern.edu>
2024-06-05 18:52:48 -0400

    all modelStock methods use LocalDate, toString returns stock values on a given date

--- src/model/Stock.java
+++ src/model/Stock.java
@@ -6,17 +6,20 @@ package model;
  */
 public interface Stock {
   /**
-   * Determines if the stock gained or lost value over the given day,
+   * Determines if the stock gained or lost value over the given period,
    * and returns the change in value.
-   * @param dateStart the date to start looking back from.
-   *                  Must be a valid date in the format "YYYY/MM/DD".
+   *
+   * @param dateStart the date to start from.
+   *                  Must be in a valid date format "YYYY/MM/DD".
    *                  Must be before dateEnd.
-   * @param dateEnd the date to end looking back from.
-   *                Must be a valid date in the format "YYYY/MM/DD".
-   *                Must be after dateStart.
+   * @param dateEnd   the date to stop at.
+   *                  Must be a valid date in the format "YYYY/MM/DD".
+   *                  Must be after dateStart.
    * @return the change in the stock's value in USD.
+   * @throws IllegalArgumentException if the date is not a valid date in the format "YYYY/MM/DD".
+   *                                  or if the start date is after the end date.
    */
-  public double gainedValue(String dateStart, String dateEnd);
+  public double gainedValue(String dateStart, String dateEnd) throws IllegalArgumentException;
 
   /**
    * Finds the total change in the stock's value over the given days.
@@ -26,13 +29,13 @@ public interface Stock {
    * @param dateStart the date to start looking back from.
    *                  Must be a valid date in the format "YYYY/MM/DD".
    *                  Must be before dateEnd.
-   * @param dateEnd the date to end looking back from.
-   *                Must be a valid date in the format "YYYY/MM/DD".
-   *                Must be after dateStart.
+   * @param dateEnd   the date to end looking back from.
+   *                  Must be a valid date in the format "YYYY/MM/DD".
+   *                  Must be after dateStart.
    * @return the total change in the stock's value in USD.
    * @throws IllegalArgumentException if the number of days is less than or equal to 0.
    */
-  public double getChange(String dateStart, String dateEnd);
+  public double getChange(String dateStart, String dateEnd) throws IllegalArgumentException;
 
   /**
    * Finds the stock's X-day moving average.
@@ -47,28 +50,43 @@ public interface Stock {
    * @throws IllegalArgumentException if the date is not a valid date in the format "YYYY/MM/DD",
    *                                  or if the number of days is less than or equal to 0.
    */
-  public double getMovingAverage(int days, String date);
+  public double getMovingAverage(int days, String date) throws IllegalArgumentException;
 
   /**
    * Finds the X-day crossovers for the stock over the given date range.
    * If the number of days is greater than the number of days the stock has been tracked,
    * the method will return the crossovers since the stock has been tracked.
    *
-   * @param days the number of days to look back to find the crossovers.
-   *             Must be greater than 0.
+   * @param days      the number of days to look back to find the crossovers.
+   *                  Must be greater than 0.
+   * @param dateStart the date to start looking back from.
+   *                  Must be a valid date in the format "YYYY/MM/DD".
+   *                  Must be before dateEnd.
+   * @param dateEnd   the date to end looking back from.
+   *                  Must be a valid date in the format "YYYY/MM/DD".
+   *                  Must be after dateStart.
+   * @return the dates of the crossovers in an array in the format "YYYY/MM/DD".
+   * @throws IllegalArgumentException if the date is not a valid date,
+   *                                  or if the number of days is less than or equal to 0.
    */
-  public String[] getCrossovers(String dateStart, String dateEnd, int days);
+  public String[] getCrossovers(String dateStart, String dateEnd, int days) throws IllegalArgumentException;
 
   /**
    * Gets the symbol of the stock.
+   *
    * @return the symbol of the stock.
    */
   public String getSymbol();
 
   /**
-   *
-    * @return the stock values as a string
+   * Gets the symbol, name, opening price, closing price, low price, and high price of the stock on a given date.
+   * @param date the date to get the stock values on.
+   *             Must be a valid date in the format "YYYY/MM/DD".
+   *             Must be a date the stock has been tracked on.
+   * @return the stock values as a string
+   * @throws IllegalArgumentException if the date is not a valid date in the format "YYYY/MM/DD"
+   *                                 or if the date is not a date the stock has been tracked on.
    */
-  public String toString();
+  public String toString(String date) throws IllegalArgumentException;
 //  public Date date();
 }
--- src/model/modelStock.java
+++ src/model/modelStock.java
@@ -19,7 +19,90 @@ public class modelStock implements Stock {
     this.apiInfo.remove(0);
   }
 
-  public double gainedValue(String dateStart, String dateEnd) {
+  public double gainedValue(String dateStart, String dateEnd) throws IllegalArgumentException {
+    if (dateStart == null || dateEnd == null) {
+      throw new IllegalArgumentException("Dates cannot be null.");
+    }
+
+    try {
+      LocalDate start = LocalDate.parse(dateStart);
+      LocalDate end = LocalDate.parse(dateEnd);
+
+      if (start.isAfter(end)) {
+        throw new IllegalArgumentException("Start date must be before end date.");
+      }
+    } catch (DateTimeParseException e) {
+      throw new IllegalArgumentException("Dates must be in a valid format.");
+    }
+
+    boolean foundStart = false;
+    boolean foundEnd = false;
+    for (String line : apiInfo) {
+      if (line.equals(dateStart)) {
+        foundStart = true;
+      } else if (line.equals(dateEnd)) {
+        foundEnd = true;
+      }
+    }
+
+    // TODO: figure out how to go back multiple days to find the last valid date
+    // if we didn't find the start date or end date, we need to go back a day
+    if (!foundStart || !foundEnd) {
+      throw new IllegalArgumentException("Did not find the start date or end date of the stock");
+    }
+
+    return 0.0;
+  }
+
+  // TODO: Implement this
+  @Override
+  public double getChange(String dateStart, String dateEnd) {
+    if (dateStart == null || dateEnd == null) {
+      throw new IllegalArgumentException("Dates cannot be null.");
+    }
+
+    try {
+      LocalDate start = LocalDate.parse(dateStart);
+      LocalDate end = LocalDate.parse(dateEnd);
+
+      if (start.isAfter(end)) {
+        throw new IllegalArgumentException("Start date must be before end date.");
+      }
+    } catch (DateTimeParseException e) {
+      throw new IllegalArgumentException("Dates must be in a valid format.");
+    }
+
+    // if not valid date, or if dateEnd is not after dateStart, use date class
+    return 0.0;
+  }
+
+  // TODO: Implement this
+  @Override
+  public double getMovingAverage(int days, String date) throws IllegalArgumentException {
+    if (days <= 0) {
+      throw new IllegalArgumentException("The number of days must be greater than 0.");
+    }
+
+    if (date == null) {
+      throw new IllegalArgumentException("Date cannot be null.");
+    }
+
+    try {
+      LocalDate.parse(date);
+    } catch (DateTimeParseException e) {
+      throw new IllegalArgumentException("Date must be in a valid format.");
+    }
+
+    return 0.0;
+  }
+
+  // TODO: Implement this
+  @Override
+  public String[] getCrossovers(String dateStart, String dateEnd, int days) {
+    if (days <= 0) {
+      throw new IllegalArgumentException("The number of days must be greater than 0.");
+    }
+
     if (dateStart == null || dateEnd == null) {
       throw new IllegalArgumentException("dates cannot be null.");
     }
@@ -35,48 +118,6 @@ public class modelStock implements Stock {
       throw new IllegalArgumentException("dates must be in a valid format.");
     }
 
-    String foundStart = null;
-    String foundEnd = null;
-    for (String s : apiInfo) {
-
-      if (s.contains(dateStart)) {
-        foundStart = s;
-      }
-      if (s.contains(dateEnd)) {
-        foundEnd = s;
-      }
-    }
-
-    // check if the given string is a weekend. if it is, go back
-    if (foundStart == null || foundEnd == null) {
-      throw new IllegalArgumentException("did not find the start date or end date of the stock");
-    }
-
-    return 0.0;
-  }
-
-  // TODO: Implement this
-  @Override
-  public double getChange(String dateStart, String dateEnd) {
-    // if not valid date, or if dateEnd is not after dateStart, use date class
-    return 0.0;
-  }
-
-  // TODO: Implement this
-  @Override
-  public double getMovingAverage(int days, String date) {
-    if (days <= 0) {
-      throw new IllegalArgumentException("The number of days must be greater than 0.");
-    }
-    return 0.0;
-  }
-
-  // TODO: Implement this
-  @Override
-  public String[] getCrossovers(String dateStart, String dateEnd, int days) {
-    if (days <= 0) {
-      throw new IllegalArgumentException("The number of days must be greater than 0.");
-    }
     return new String[0];
   }
 
@@ -85,9 +126,19 @@ public class modelStock implements Stock {
     return symbol;
   }
 
-  // toString prints out stock values
+  // toString prints out stock values on a given date
   @Override
-  public String toString() {
+  public String toString(String date) {
+    if (date == null) {
+      throw new IllegalArgumentException("Date cannot be null.");
+    }
+
+    try {
+      LocalDate.parse(date);
+    } catch (DateTimeParseException e) {
+      throw new IllegalArgumentException("Date must be in a valid format.");
+    }
+
     return "";
   }
 
