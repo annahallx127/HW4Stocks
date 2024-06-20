b03b86ba81fb2c7fb953d474e768483531dd8029
Anna Hall <hall.anna@northeastern.edu>
2024-06-20 16:18:29 -0400

    Additions to JFrameView

--- src/controller/JFrameMain.java
+++ src/controller/JFrameMain.java
@@ -6,9 +6,9 @@ import model.Model;
 
 public class JFrameMain {
 
-    public static void main(String[] args) {
-        Model model = new ModelImpl();
-        JFrameView view = new JFrameView();
-        new JFrameControllerImpl(model, view);
-    }
+  public static void main(String[] args) {
+    Model model = new ModelImpl();
+    JFrameView view = new JFrameView();
+    new JFrameControllerImpl(model, view);
+  }
 }
